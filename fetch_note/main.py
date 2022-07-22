2081241cff83ab8c6728653bb98246179925f50b
thetlwinlwin <minthantlwin8@gmail.com>
2022-07-22 17:27:58 +0630

    add get title and test it

--- fetch_note/main.py
+++ fetch_note/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from google.oauth2 import service_account
+from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
 from fetch_note.settings import my_setting
@@ -20,21 +21,43 @@ app.add_middleware(
 )
 
 
-def get_note() -> str:
+def get_service()->Credentials:
     info = my_setting.dict(exclude={"doc_id"})
     creds = service_account.Credentials.from_service_account_info(info)
+    return creds
+    
+
+def get_doc_content()->str:
+    my_cred = get_service()
     service = build(
         "docs",
         "v1",
-        credentials=creds,
+        credentials=my_cred,
     )
     # pylint: disable=maybe-no-member
     document = service.documents().get(documentId=my_setting.doc_id).execute()
     doc_content = document.get("body").get("content")
     return read_structural_elements(doc_content)
 
+def get_doc_title()->str:
+    my_cred = get_service()
+    service = build(
+        "docs",
+        "v1",
+        credentials=my_cred,
+    )
+    # pylint: disable=maybe-no-member
+    document = service.documents().get(documentId=my_setting.doc_id).execute()
+    return document.get('title')
+
 
 @app.get("/")
-def index():
-    text = get_note()
-    return {"x": f"{text}"}
+def get_content():
+    content = get_doc_content()
+    return {"x": f"{content}"}
+
+
+@app.get('/get_title')
+def get_title():
+    title = get_doc_title()
+    return {"Title":f"{title}"}
--- fetch_note/test/test_main.py
+++ fetch_note/test/test_main.py
@@ -1,10 +1,11 @@
-from fastapi import Response
 from fastapi.testclient import TestClient
+from requests import Response
 from fetch_note.main import app
 
 
 client :TestClient = TestClient(app=app)
 
 def test_main():
-    res:Response = client.get('/')
+    res:Response = client.get('/get_title')
     assert res.status_code == 200
+    assert res.json() ==  {"Title": "Love Notes"}
